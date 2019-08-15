38109a0b1906cf389a32d3c7958407447654e753
Reemet Paabo <reemet.paabo@testreel.com>
2019-08-15 11:20:45 +0300

    as

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-// app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
 app.use(express.static(__dirname));
 
