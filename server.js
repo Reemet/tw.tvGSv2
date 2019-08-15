cf182f6eecc6a8e7d0839a2b9e76a3a3b209e42e
Reemet Paabo <reemet.paabo@testreel.com>
2019-08-15 10:53:29 +0300

    use ACAO

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 
 // the __dirname is the current directory from where the script is running
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.static(__dirname));
 
 
